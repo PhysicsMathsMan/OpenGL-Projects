4083d63afc34e75d6b6e30286a4d596f581baf1e
PhysicsMathsMan <mr.adithya.atg@gmail.com>
2017-03-29 16:10:43 +0530

    Black on white

--- sample/main.c
+++ sample/main.c
@@ -1,11 +1,10 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
  
-void renderFunction()
-{
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+void renderFunction() {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(0.0, 0.0, 0.0);
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
     glBegin(GL_POLYGON);
         glVertex2f(-0.5, -0.5);
@@ -15,8 +14,10 @@ void renderFunction()
     glEnd();
     glFlush();
 }
-int main(int argc, char** argv)
-{
+
+
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(500,500);
