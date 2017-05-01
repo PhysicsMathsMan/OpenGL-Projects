eb7aa272bc8ef1c4af7d790c8bec31642eb1c304
petgoldfish <g.raghav.sai@gmail.com>
2017-05-01 22:44:58 +0530

    Fully modularised sat code

--- cg-project/sprites/sat.c
+++ cg-project/sprites/sat.c
@@ -1,36 +1,36 @@
-void drawSat(int x, int y, float scale)
+void drawSat(float x, float y, float scale)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     // Body
     glColor3f(226 / 255.0, 226 / 255.0, 72 / 255.0);
     glBegin(GL_QUADS);
-    glVertex2f(300.0 * scale, 300.0 * scale);
-    glVertex2f(300.0 * scale, 500.0 * scale);
-    glVertex2f(500.0 * scale, 500.0 * scale);
-    glVertex2f(500.0 * scale, 300.0 * scale);
+    glVertex2f(x * scale, y * scale);
+    glVertex2f(x * scale, (y + 200.0) * scale);
+    glVertex2f((x + 200.0) * scale, (y + 200.0) * scale);
+    glVertex2f((x + 200.0) * scale, y * scale);
     glEnd();
     // Connectors
     glColor3f(81 / 255.0, 99 / 255.0, 127 / 255.0);
     glBegin(GL_LINES);
-    glVertex2f(500.0 * scale, 400.0 * scale);
-    glVertex2f(550.0 * scale, 400.0 * scale);
-    glVertex2f(300.0 * scale, 400.0 * scale);
-    glVertex2f(250.0 * scale, 400.0 * scale);
+    glVertex2f((x + 200.0) * scale, (y + 100.0) * scale);
+    glVertex2f((x + 250.0) * scale, (y + 100.0) * scale);
+    glVertex2f(x * scale, (y + 100.0) * scale);
+    glVertex2f((x - 50.0) * scale, (y + 100.0) * scale);
     glEnd();
     // Panels
     int tilt = 10;
     glColor3f(29 / 255.0, 92 / 255.0, 193 / 255.0);
     glBegin(GL_QUADS);
-    glVertex2f((250.0 - tilt) * scale, 350.0 * scale);
-    glVertex2f((250.0 + tilt) * scale, 450.0 * scale);
-    glVertex2f((50.0 + tilt) * scale, 450.0 * scale);
-    glVertex2f((50.0 - tilt) * scale, 350.0 * scale);
+    glVertex2f((x - 50.0 - tilt) * scale, (y + 50.0) * scale);
+    glVertex2f((x - 50.0 + tilt) * scale, (y + 150.0) * scale);
+    glVertex2f((x - 250.0 + tilt) * scale, (y + 150.0) * scale);
+    glVertex2f((x - 250.0 - tilt) * scale, (y + 50.0) * scale);
     glEnd();
     glBegin(GL_QUADS);
-    glVertex2f((550.0 - tilt) * scale, 350.0 * scale);
-    glVertex2f((550.0 + tilt) * scale, 450.0 * scale);
-    glVertex2f((750.0 + tilt) * scale, 450.0 * scale);
-    glVertex2f((750.0 - tilt) * scale, 350.0 * scale);
+    glVertex2f((x + 250.0 - tilt) * scale, (y + 50.0) * scale);
+    glVertex2f((x + 250.0 + tilt) * scale, (y + 150.0) * scale);
+    glVertex2f((x + 450.0 + tilt) * scale, (y + 150.0) * scale);
+    glVertex2f((x + 450.0 - tilt) * scale, (y + 50.0) * scale);
     glEnd();
 
     glFlush();
